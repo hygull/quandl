476cd9983e83cdb38c096ea4da83acad075c12b4
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2018-05-15 15:32:15 +0530

    16 saved obtained data as csv

--- src/gold_price_london_fixing/gold_price_london_fixing_as_pandas.py
+++ src/gold_price_london_fixing/gold_price_london_fixing_as_pandas.py
@@ -8,4 +8,7 @@ quandl.ApiConfig.api_key = conf.API_KEY;
 
 data = quandl.get("LBMA/GOLD", authtoken="2Evs3E8J_LduFKbNF6Gw", returns='pandas', start_date='01-01-2014', end_date="01-01-2017");
 print data;
-print type(data); 
+print type(data); # <class 'pandas.core.frame.DataFrame'>
+
+# Save te obtained data to csv
+data.to_csv("GOLD-Gold-Price-London-Fixing.csv", sep='\t', encoding='utf-8');
